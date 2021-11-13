cc80e5a1865980cdf990c144f7022831cfdccb9c
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-11-13 12:35:37 +0600

    Стилизация блоков items-block , item

--- drink-app/src/app/app.module.ts
+++ drink-app/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemsBlockComponent } from './items-block/items-block.component';
+import { ItemComponent } from './items-block/item/item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsBlockComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule
---
+++ drink-app/src/app/items-block/items-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-block',
+  templateUrl: './items-block.component.html',
+  styleUrls: ['./items-block.component.css']
+})
+export class ItemsBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
